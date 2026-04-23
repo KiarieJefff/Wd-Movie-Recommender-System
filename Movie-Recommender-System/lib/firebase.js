c88ecf0b024ae71af935d20882d9936465d29a1f
Kiarie <kamaukiarie254@gmail.com>
2026-04-23 13:26:08 +0300

    Security: Remove Firebase credentials from code and update package.json

--- Movie-Recommender-System/lib/firebase.js
+++ Movie-Recommender-System/lib/firebase.js
@@ -3,14 +3,7 @@ import { getDatabase } from 'firebase/database';
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBsknabI_XdqVXihz9iJCxDwhYpguar1zg",
-  authDomain: "imdb-proj-1.firebaseapp.com",
-  databaseURL: "https://imdb-proj-1-default-rtdb.firebaseio.com",
-  projectId: "imdb-proj-1",
-  storageBucket: "imdb-proj-1.firebasestorage.app",
-  messagingSenderId: "260485455352",
-  appId: "1:260485455352:web:21d2c708d165ced3acca0d",
-  measurementId: "G-07LNW541S2"
+  // Add your Firebase config here
 };
 
 const app = initializeApp(firebaseConfig);
